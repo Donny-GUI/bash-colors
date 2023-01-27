d6801f4f75737636d4d48b5660a08126eaab836e
shipit <108424001+Donny-GUI@users.noreply.github.com>
2023-01-27 11:34:23 -0800

    Create colors.sh

---
+++ colors.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+########################
+#       Color
+########################
+
+#[  foreground  ]
+
+endc="\033[0m"
+black="\033[0;30m"
+blue="\033[0;34m"
+cyan="\033[0;36m"
+purple="\033[0;35m"
+brown="\033[0;33m"
+lgray="\033[0;37m"
+dgray="\033[1;30m"
+lblue="\033[1;34m"
+lgreen="\033[1;32m"
+lcyan="\033[1;36m"
+lpurple="\033[1;35m"
+yellow="\033[1;33m"
+white="\033[1;37m"
+
+#[  background  ]
+
+bg_black="\033[0;40m"
+bg_blue="\033[0;44m"
+bg_cyan="\033[0;46m"
+bg_purple="\033[0;45m"
+bg_brown="\033[0;43m"
+bg_lgray="\033[0;47m"
+bg_dgray="\033[1;40m"
+bg_lblue="\033[1;44m"
+bg_lgreen="\033[1;42m"
+bg_lcyan="\033[1;46m"
+bg_lpurple="\033[1;45m"
+bg_yellow="\033[1;43m"
+bg_white="\033[1;47m"
+
+#[  font    ]
+
+bold="\033[1m"
+dim="\033[2m"
+italic="\033[3m"
+underline="\033[4m"
+blink="\033[5m"
